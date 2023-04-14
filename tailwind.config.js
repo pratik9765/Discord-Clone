56dedb78c91352caec0b28486988438154ebd17f
pratik9765 <pratikbokde9765@gmail.com>
2023-04-14 23:48:49 +0530

    commit message

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Abril: ['Abril Fatface', 'cursive'],
+        Arimo: ['Arimo', 'sans-serif'],
+        Lilita:['Lilita One'],
+        Secular: ['Secular One']
+    },
+  },
+  plugins: [],
+}
+}
